89bc4246d454101188c0015d9cef2b71510133e9
Fernando <fernando.ti@grupocarvalhogestao.com.br>
2026-02-15 13:50:22 -0300

    secao 23

---
+++ secao_23/script/1_script.js
@@ -0,0 +1,6 @@
+function executar(){
+    console.log('Executar')
+}
+//const botao = document.getElementById('botao')
+const botao = document.querySelector('[botao-acao]')
+botao.addEventListener('click',executar, )
