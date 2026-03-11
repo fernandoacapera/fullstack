8e9ea90c954b7a7873f56c152cc3170d18447025
fernandoacapera <fernandocapera2018@outlook.com>
2026-03-11 20:29:58 -0300

    273

---
+++ secao_26/src/modules/basic-types/type-any.ts
@@ -0,0 +1,9 @@
+export function handleFileUpload(file: any){
+  console.log(`Nome: ${file.name}`)
+  console.log(`Tamanho: ${file.size}`)
+}
+const file = {
+  name: 'lista_de_funcionarios.txt',
+  size: 456123789
+}
+handleFileUpload(file);
